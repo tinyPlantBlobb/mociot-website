9a07c05c46c843c5fd913db3798212183fee9133
PlantPalFynn <PlantPalFynn@gmail.com>
2022-12-13 19:12:11 +0100

    add git ignore, fix automatic rezizing of canvas

--- animation.js
+++ animation.js
@@ -1,36 +1,85 @@
+var canvas = document.querySelector( 'canvas' );
+var context = canvas.getContext( '2d' );
+function setup() {
 
-function initiate(){
-    var elem=document.getElementById('canvas');
-    canvas= elem.getContext('2d');
-    window.addEventListener('resize', resize, false);
-    width = innerWidth;
-    height = outerHeight;
+    resize();
+    window.addEventListener( 'resize', resize );
 }
-
-function resize(){
-    width = innerWidth;
-    height = innerHeight;
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    canvas.fillText(window.innerWidth, 300,300);
-    canvas.fillRect(0,0,200,200);
-    canvas.strokeStyle = "#000099";
-    canvas.strokeRect(0,0,width,height);
-
+function resize() {
+    width = canvas.width = window.innerWidth;
+    height = canvas.height = window.innerHeight;
 }
-function draw(){
-    //var contxt = (a canvas context);
-    canvas.clearRect(0,0,width,height);
-    canvas.font="bold 12px arial";
-    canvas.textAlign="start";
-    canvas.fillText(window.innerWidths, 0, 0);
-    var inp = document.getElementById('input');
-    let inputs = inp.value;
-    canvas.font="bold 12px arial";
-    canvas.textAlign="start";
-    canvas.fillText(inputs, 100, 100)
-    inp.value = '';
-}
-window.addEventListener('load', initiate, false);
+setup();
 
-window.addEventListener('change', draw, false);
+
+//  var cnvs=document.querySelector('canvas');
+// var cxt= cnvs.getContext('2d');
+// height,
+// width,
+
+
+// function initiate(){
+//     resize();
+//     window.addEventListener('load', initiate, false);
+//     window.addEventListener('change', draw, false);
+//     window.addEventListener('resize', resize );
+// }
+
+// function resize(){
+
+//     heigth = cxt.height = window.innerHeight;
+
+//     width = cxt.width = window.innerWidth;
+    
+    
+    
+    
+//     cxt.fillText(window.innerWidth, 300,300);
+    
+//     cxt.fillRect(0,0,200,200);
+    
+//     cxt.strokeStyle = "#000099";
+    
+//     cxt.strokeRect(0,0,width,height);
+
+// }
+// function draw(){
+//     //var contxt = (a canvas context);
+    
+//     cxt.clearRect(0,0,width,height);
+    
+//     cxt.font="bold 12px arial";
+    
+//     cxt.textAlign="start";
+    
+//     cxt.fillText(window.innerWidths, 0, 0);
+//     var inp = document.getElementById('input');
+//     let inputs = inp.value;
+    
+//     cxt.font="bold 12px arial";
+    
+//     cxt.textAlign="start";
+    
+//     cxt.fillText(inputs, 100, 100)
+//     inp.value = '';
+// }
+
+
+// audio control 
+togglevisibility.addEventListener ('click',
+    function() {           // anonyme Funktion
+		btn = document.querySelector("#btn_visibility");
+		snippets = document.querySelector("audioplayer");
+		if (btn.innerHTML == "Hide"){
+			snippets.style.visibility="hidden";
+			btn.innerHTML = "None";
+		} else if(btn.innerHTML == "None"){
+			snippets.style.display = "none";
+			btn.innerHTML = "Show";
+		} else{
+			btn.innerHTML = "Hide";
+			snippets.style.display = "grid";
+			snippets.style.visibility = "visible";
+		}
+    }, 
+    true);
